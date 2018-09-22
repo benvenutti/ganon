be5d6f16fe5b398aab30f445ba7cf979991b47d6
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-22 15:45:59 +0200

    Style changes

--- src/utils/include/utils/safequeue.hpp
+++ src/utils/include/utils/safequeue.hpp
@@ -13,15 +13,18 @@ class SafeQueue
 public:
     void push( T t )
     {
-        std::lock_guard<std::mutex> lock( m_mutex );
+        std::lock_guard< std::mutex > lock( m_mutex );
         m_queue.push( std::move( t ) );
     }
 
     std::optional< T > pop()
     {
-        std::lock_guard<std::mutex> lock( m_mutex );
+        std::lock_guard< std::mutex > lock( m_mutex );
+
         if ( m_queue.empty() )
+        {
             return std::nullopt;
+        }
 
         std::optional< T > val( std::move( m_queue.front() ) );
         m_queue.pop();
@@ -30,17 +33,17 @@ public:
 
     auto size() const
     {
-        std::lock_guard<std::mutex> lock( m_mutex );
+        std::lock_guard< std::mutex > lock( m_mutex );
         return m_queue.size();
     }
 
     bool empty() const
     {
-        std::lock_guard<std::mutex> lock( m_mutex );
+        std::lock_guard< std::mutex > lock( m_mutex );
         return m_queue.empty();
     }
 
 private:
-    std::queue<T> m_queue;
+    std::queue< T > m_queue;
     mutable std::mutex m_mutex;
 };
