45490f48d3015d870141242e5fddf55b625c2243
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-22 14:58:49 +0200

    Style and rename update

--- src/utils/include/utils/safequeue.hpp
+++ src/utils/include/utils/safequeue.hpp
@@ -2,35 +2,38 @@
 #include <queue>
 
 template <class T>
-class SafeQueue{
-    public:
-        void push(T t){
-            std::lock_guard<std::mutex> lock(m);
-            q.push(t);
-        }
+class SafeQueue
+{
+public:
+    void push( T t )
+    {
+        std::lock_guard<std::mutex> lock( m_mutex );
+        m_queue.push( t );
+    }
 
-        T pop(){
-            std::lock_guard<std::mutex> lock(m);
-            if ( q.empty() )
-                return T();
-            T val = q.front();
-            q.pop();
-            return val;
-        }
+    T pop()
+    {
+        std::lock_guard<std::mutex> lock( m_mutex );
+        if ( m_queue.empty() )
+            return T();
+        T val = m_queue.front();
+        m_queue.pop();
+        return val;
+    }
 
-        int size() const
-        {
-            std::lock_guard<std::mutex> lock(m);
-            return q.size();
-        }
+    int size() const
+    {
+        std::lock_guard<std::mutex> lock( m_mutex );
+        return m_queue.size();
+    }
 
-        bool empty() const
-        {
-            std::lock_guard<std::mutex> lock(m);
-            return q.empty();
-        }
+    bool empty() const
+    {
+        std::lock_guard<std::mutex> lock( m_mutex );
+        return m_queue.empty();
+    }
 
-    private:
-        std::queue<T> q;
-        mutable std::mutex m;
+private:
+    std::queue<T> m_queue;
+    mutable std::mutex m_mutex;
 };
