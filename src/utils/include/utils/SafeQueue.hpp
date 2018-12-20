c0a290f79a34c58daa44cc43fd3f94cc2cc5966f
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2018-12-20 21:20:03 +0100

    Update SafeQueue
    When updating the branch by merging the master branch in, the SafeQueue
    file was deleted, so the improvements got lost. Here the safe queue is
    getting back its values.

--- src/utils/include/utils/SafeQueue.hpp
+++ src/utils/include/utils/SafeQueue.hpp
@@ -1,41 +1,62 @@
 #pragma once
 
 #include <mutex>
+#include <optional>
 #include <queue>
+#include <type_traits>
+#include <utility>
 
-template < class T >
+template < class T,
+           class Enabled = std::enable_if_t< std::is_move_assignable< T >::value && //
+                                             std::is_move_constructible< T >::value > >
 class SafeQueue
 {
-private:
-    std::queue< T > q;
-    std::mutex      m;
-
 public:
-    void push( T t )
+    void push( const T& value )
     {
-        std::lock_guard< std::mutex > lock( m );
-        q.push( t );
+        std::lock_guard< std::mutex > lock( m_mutex );
+        m_queue.push( value );
     }
 
-    T pop()
+    void push( T&& value )
     {
-        std::lock_guard< std::mutex > lock( m );
-        if ( q.empty() )
-            return T();
-        T val = q.front();
-        q.pop();
-        return val;
+        std::lock_guard< std::mutex > lock( m_mutex );
+        m_queue.push( value );
     }
 
-    int size()
+    template < class... Args >
+    void emplace( Args&&... args )
     {
-        std::lock_guard< std::mutex > lock( m );
-        return q.size();
+        std::lock_guard< std::mutex > lock( m_mutex );
+        m_queue.emplace( std::forward< Args >( args )... );
     }
 
-    bool empty()
+    std::optional< T > pop()
     {
-        std::lock_guard< std::mutex > lock( m );
-        return q.empty();
+        std::lock_guard< std::mutex > lock( m_mutex );
+        if ( m_queue.empty() )
+        {
+            return std::nullopt;
+        }
+
+        auto value = std::make_optional( std::move( m_queue.front() ) );
+        m_queue.pop();
+        return value;
     }
+
+    auto size() const
+    {
+        std::lock_guard< std::mutex > lock( m_mutex );
+        return m_queue.size();
+    }
+
+    bool empty() const
+    {
+        std::lock_guard< std::mutex > lock( m_mutex );
+        return m_queue.empty();
+    }
+
+private:
+    std::queue< T >    m_queue;
+    mutable std::mutex m_mutex;
 };
